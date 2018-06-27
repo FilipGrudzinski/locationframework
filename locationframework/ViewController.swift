7c598d7d0bdf581e5e52b4a17b301eff6640694e
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-06-27 22:43:20 +0200

    user location track

--- locationframework/ViewController.swift
+++ locationframework/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 
-class ViewController: UIViewController,  CLLocationManagerDelegate {
+class ViewController: UIViewController,  CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
     
     var locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,7 +36,26 @@ class ViewController: UIViewController,  CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        print(locations)
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        //print(locations)
+        
         
     }
 
